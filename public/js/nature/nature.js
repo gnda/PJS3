fa9862939694e4b5e3110dae4292fd39867d27c2
K Nanda <kevin.nandacoumar@gmail.com>
2017-11-23 02:47:56 +0100

    Progression partie nature slides

---
+++ public/js/nature/nature.js
@@ -0,0 +1,10 @@
+/*(function(){
+	var boutonsSlide = document.getElementsByClassName("slider-change");
+
+	var changerCouleurBouton = function(){
+		alert("aaa");
+	};
+
+	for (var i = 0; i < boutonsSlide.length; i++)
+		boutonsSlide[i].addEventListener('click', changerCouleurBouton, false);
+})();*/
