e5b0119971f86bf1d4951e36200e6e40326f40a1
knda <kevin.nandacoumar@gmail.com>
2017-11-24 09:15:57 +0100

    Modification du fontello (ajout fleches gauche/droite) pour la partie nature

--- public/js/nature/nature.js
+++ public/js/nature/nature.js
@@ -1,4 +1,9 @@
 $("#owl-nature").owlCarousel({
 	dots: true,
-	items: 1
+	items: 1,
+	nav: true,
+	navText: [
+	   "<i class='icon-left-open-big'></i>",
+	   "<i class='icon-right-open-big'></i>"
+	]
 });
