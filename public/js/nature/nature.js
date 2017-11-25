09dacfda452269467b16911b76beb9f31f43d964
K Nanda <kevin.nandacoumar@gmail.com>
2017-11-26 00:36:20 +0100

    Partie nature terminée (pas responsive pr le moment)

--- public/js/nature/nature.js
+++ public/js/nature/nature.js
@@ -8,3 +8,38 @@ $("#owl-nature").owlCarousel({
 	   "<i class='icon-right-open-big'></i>"
 	]
 });
+
+$(function(){
+	if($("#owl-nature .owl-item").length == 0)
+		actualiserCarousel("#owl-nature",".lacs","#nature-content");
+});
+
+$(".link-lacs").click(function(){
+	actualiserCarousel("#owl-nature",".lacs","#nature-content");
+});
+
+$(".link-mil-roc").click(function(){
+	actualiserCarousel("#owl-nature",".mil_roc","#nature-content");
+});
+
+$(".link-plateaux").click(function(){
+	actualiserCarousel("#owl-nature",".plateaux","#nature-content");
+});
+
+/*	classeItem --> classe du groupe d'éléments à ajouter
+ *	nomContainer --> nom de la div qui contient les éléments à ajouter
+ */
+function actualiserCarousel(nomCarousel, classeItem, nomContainer){
+	viderCarousel(nomCarousel);
+	$(nomContainer+" "+classeItem).each(function(){
+		$(nomContainer).append($(this).clone());
+		$(nomCarousel).owlCarousel("add", this).owlCarousel("refresh");
+	});
+}
+
+function viderCarousel(nomCarousel){
+	var nbObj = $(nomCarousel+' .owl-item').not('.cloned').length;
+	for(var i = 0; i < nbObj; i++)
+		$(nomCarousel).trigger("remove.owl.carousel", i);
+	$(nomCarousel).owlCarousel("refresh");
+}
