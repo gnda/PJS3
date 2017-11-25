f990066542f3dc50c71e8109bdba4e451a0d19f9
K Nanda <kevin.nandacoumar@gmail.com>
2017-11-26 00:52:33 +0100

    le logo renvoie bien vers l'accueil

--- app/routes.php
+++ app/routes.php
@@ -3,9 +3,10 @@
 
 $app->group('', function() use ($container) {
     
-    $this->get('/', \App\Controllers\Home\HomeController::class . ':home')->setName('home');
+    $this->get('/home', \App\Controllers\Home\HomeController::class . ':home')->setName('home');
+	$this->get('/', \App\Controllers\Home\HomeController::class . ':home');
     $this->get('', \App\Controllers\Home\HomeController::class . ':home');    
-    
+
 	$this->get('/nature', \App\Controllers\Home\HomeController::class . ':nature')->setName('nature');
 	$this->get('/histoire', \App\Controllers\Home\HomeController::class . ':histoire')->setName('histoire');
 	$this->get('/grandes_villes', \App\Controllers\Home\HomeController::class . ':grandes_villes')->setName('grandes_villes');
