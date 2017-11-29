e18fe5ab9218ca2932b87caa3988457e3d312efe
K Nanda <kevin.nandacoumar@gmail.com>
2017-11-29 06:37:13 +0100

    Refonte nature en cours

--- public/js/nature/nature.js
+++ public/js/nature/nature.js
@@ -1,4 +1,34 @@
+var slideIndex = 1;
+
 document.addEventListener('DOMContentLoaded', function() {
+	showDivs(slideIndex);
+});
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function currentDiv(n) {
+  showDivs(slideIndex = n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("item");
+ /* var dots = document.getElementsByClassName("demo");*/
+  if (n > x.length)
+	  slideIndex = 1    
+  if (n < 1)
+	  slideIndex = x.length
+  for (i = 0; i < x.length; i++)
+	 x[i].style.display = "none";  
+  /*for (i = 0; i < dots.length; i++)
+	 dots[i].className = dots[i].className.replace(" w3-white", "");*/
+  x[slideIndex-1].style.display = "block";  
+ /* dots[slideIndex-1].className += " w3-white";*/
+}
+
+/*document.addEventListener('DOMContentLoaded', function() {
 	if(document.querySelector("#slider-nature .slider-item") == null)
 		actualiserSlider("#slider-nature",".lacs","#nature-content");
 	document.querySelector(".link-lacs").addEventListener("click", function(){
@@ -19,7 +49,12 @@ function afficherItems(nomSlider, classeItem){
 	var divs = document.querySelectorAll(nomSlider+" "+classeItem), i;
 	for (i = 0; i < divs.length; ++i)
 	  divs[i].display = "inline";
-}
+}*/
+
+
+
+
+
 
 /*$("#owl-nature").owlCarousel({
 	dots: true,
